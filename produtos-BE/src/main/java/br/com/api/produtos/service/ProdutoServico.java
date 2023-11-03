e0ea8206e6f4e5fa6433105b7d98767849250c6b
Lucas Almeida <lucas.almeidasilva49@gmail.com>
2023-11-03 12:11:00 -0300

    criando service e metodo de listar

--- produtos-BE/src/main/java/br/com/api/produtos/service/ProdutoServico.java
+++ produtos-BE/src/main/java/br/com/api/produtos/service/ProdutoServico.java
@@ -1,5 +1,20 @@
 package br.com.api.produtos.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.repositorio.ProdutoRepositorio;
+
+@Service
 public class ProdutoServico {
     
+    @Autowired
+    private ProdutoRepositorio produto_Repositorio;
+
+
+    //Listar todos os produtos
+    public Iterable<ProdutoModelo> listar(){
+        return produto_Repositorio.findAll();
+    }
 }
