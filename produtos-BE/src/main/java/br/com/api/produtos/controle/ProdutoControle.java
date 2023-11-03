c7d8a6af5b68c57900b7f79fae2c7415b689638d
Lucas Almeida <lucas.almeidasilva49@gmail.com>
2023-11-03 15:00:28 -0300

    metodo cadastrar

--- produtos-BE/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ produtos-BE/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -1,15 +1,32 @@
 package br.com.api.produtos.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.service.ProdutoServico;
 
 @RestController
 
 public class ProdutoControle {
     
+    @Autowired
+    private ProdutoServico produtoServico;
 
-    @GetMapping("/produtos")
-    public String rota(){
-        return "API DE PRODUTOS OK!";
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModelo> listar(){
+        return produtoServico.listar();
     }
+
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo produtoModelo){
+        return produtoServico.cadastrar(produtoModelo);
+    }
+    
 }
--- produtos-BE/src/main/java/br/com/api/produtos/service/ProdutoServico.java
+++ produtos-BE/src/main/java/br/com/api/produtos/service/ProdutoServico.java
@@ -1,9 +1,12 @@
 package br.com.api.produtos.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.modelo.RespostaModelo;
 import br.com.api.produtos.repositorio.ProdutoRepositorio;
 
 @Service
@@ -12,9 +15,30 @@ public class ProdutoServico {
     @Autowired
     private ProdutoRepositorio produto_Repositorio;
 
+    @Autowired
+    private RespostaModelo resposta_Modelo;
+
 
     //Listar todos os produtos
     public Iterable<ProdutoModelo> listar(){
         return produto_Repositorio.findAll();
     }
+
+    //cadastrar produto
+    public ResponseEntity<?> cadastrar(ProdutoModelo produto){
+        if(produto.getNome().equals("")){
+            resposta_Modelo.setMensagem("O nome precisa ser preenchido");
+            return new ResponseEntity<>(resposta_Modelo, HttpStatus.BAD_REQUEST);
+                            
+        }else if(produto.getMarca().equals("")){
+            resposta_Modelo.setMensagem("A marca Precisa ser preenchida");
+            return new ResponseEntity<>(resposta_Modelo, HttpStatus.BAD_REQUEST);
+
+        }else{
+            return new ResponseEntity<>(produto_Repositorio.save(produto), HttpStatus.OK);
+        }
+    }
+
+    
+
 }
