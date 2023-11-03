b7928f9de15919ffa932279fc5229285baa84f08
Lucas Almeida <lucas.almeidasilva49@gmail.com>
2023-11-03 12:02:26 -0300

    criando controller

--- produtos-BE/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ produtos-BE/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -1,5 +1,15 @@
 package br.com.api.produtos.controle;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class ProdutoControle {
     
+
+    @GetMapping("/produtos")
+    public String rota(){
+        return "API DE PRODUTOS OK!";
+    }
 }
