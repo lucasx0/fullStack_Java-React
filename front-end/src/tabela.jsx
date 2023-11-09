a851561facb197738e5947abb08f16ed57bbbb8d
Lucas Almeida <lucas.almeidasilva49@gmail.com>
2023-11-09 13:43:35 -0300

    style em tabela

--- front-end/src/tabela.jsx
+++ front-end/src/tabela.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function Tabela(){
     return(
-        <table>
+        <table className="table">
             <thead>
                 <tr>
                     <th>#</th>
