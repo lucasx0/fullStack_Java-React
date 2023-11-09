437fbd3e2d70370e3aaf820200f8b87f6446b2ac
Lucas Almeida <lucas.almeidasilva49@gmail.com>
2023-11-09 13:36:35 -0300

    estilizando formulario

--- front-end/src/formulario.jsx
+++ front-end/src/formulario.jsx
@@ -3,13 +3,13 @@ import React from "react";
 export default function Formulario(){
     return(
         <form action="">
-            <input type="text"  placeholder="Nome"/>
-            <input type="text" placeholder="Marca"/>
+            <input type="text"  placeholder="Nome" className="form-control"/>
+            <input type="text" placeholder="Marca" className="form-control"/>
 
-            <input type="button" value="Cadastrar" />
-            <input type="button" value="Alterar" />
-            <input type="button" value="Excluir" />
-            <input type="button" value="Cancelar" />
+            <input type="button" value="Cadastrar" className="btn btn-primary"/>
+            <input type="button" value="Alterar" className="btn btn-warning"/>
+            <input type="button" value="Excluir" className="btn btn-danger"/>
+            <input type="button" value="Cancelar" className="btn btn-secondary"/>
         </form>
     )
 }
